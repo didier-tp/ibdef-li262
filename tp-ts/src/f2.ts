e53fe21a789ae71c731f6df7f7484252ed32ad40
didier <d>
2019-12-16 14:10:12 +0100

    s

--- tp-ts/src/f2.ts
+++ tp-ts/src/f2.ts
@@ -1,1 +1,8 @@
-console.log("f2");
+//console.log("f2");
+var tableau : string[]  = [];
+tableau.push("javascript");
+tableau.push("angular");
+tableau.push("typescript");
+for(let s of tableau){
+    console.log(s.toUpperCase())
+}
