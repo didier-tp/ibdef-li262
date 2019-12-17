7dfe4a619c31f4b9cf97e08a464ebd2b2b304284
didier <d>
2019-12-17 10:44:42 +0100

    debut tva

--- my-app/src/app/basic/tva/tva.component.ts
+++ my-app/src/app/basic/tva/tva.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TvaComponent implements OnInit {
 
-  constructor() { }
+  ht : number ;
+  taux : number ;
+  listeTaux : number[] = [ 5 , 10 , 20];
+  tva  : number ;
+  ttc : number ;
+
+  constructor() {
+    this.taux = this.listeTaux[0];
+   }
+
+   onCaclulerTvaEtTtc(){
+     this.tva = this .ht * this.taux / 100;
+     this.ttc = Number(this.tva) + Number(this.ht);
+   }
 
   ngOnInit() {
   }
