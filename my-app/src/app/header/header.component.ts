16715759c94ad5bd7f198da01f2ababcf7191407
didier <d>
2019-12-18 11:47:15 +0100

    s

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
+  
+  ngOnChanges(){
+    console.log("ngOnChanges() appele avec titre = " + this.titre);
+  }
 
   @Input()
   titre : string = "titre par defaut";
 
-  constructor() { }
+  constructor() {
+    console.log("constructor() appele avec titre = " + this.titre);
+   }
 
   ngOnInit() {
+    console.log("ngOnInit() appele avec titre = " + this.titre);
   }
 
 }
