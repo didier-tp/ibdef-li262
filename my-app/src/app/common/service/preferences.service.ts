a84f038cfa5bb3f8218a1ec0ad41070e3a163fe9
didier <d>
2019-12-19 16:44:21 +0100

    s

--- my-app/src/app/common/service/preferences.service.ts
+++ my-app/src/app/common/service/preferences.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreferencesService {
 
-  public couleurFondPreferee : string = "yellow"; //default value
+  //public couleurFondPreferee : string = "yellow"; //default value
+
+  public bsCouleurFond : BehaviorSubject<string>
+              = new BehaviorSubject<string>("yellow");
+     
+  public majCouleurFond(nouvelleCouleur : string){
+    this.bsCouleurFond.next(nouvelleCouleur);
+    //.next() change la valeur interne du BehaviorSubject (Object)
+    //et ça va automatiquement redéclenché toutes les callbacks
+    //enregistrées via subscribe
+  }            
 
   constructor() { }
 }
