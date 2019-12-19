599e0534e165929e50b0a17e5ec1b4c44ef6b4b8
didier <d>
2019-12-19 15:21:41 +0100

    routing children

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { BasicComponent } from './basic/basic.component';
 import { ConversionComponent } from './conversion/conversion.component';
+import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
+import { TvaComponent } from './basic/tva/tva.component';
 
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full'},
   { path: 'login', component: LoginComponent } ,
-  { path: 'basic', component: BasicComponent } ,
+  { path: 'basic', component: BasicComponent ,
+  children: [
+   { path: 'tva', component: TvaComponent },
+   { path: 'calculatrice', component: CalculatriceComponent },
+   { path: '', redirectTo: 'tva', pathMatch: 'prefix'}
+   ]
+  } ,
   { path: 'conversion', component: ConversionComponent } 
 ];
 
