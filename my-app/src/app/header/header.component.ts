b15d0c5666ad52913052b64c6013f9d9939fc684
didier <d>
2019-12-20 16:52:54 +0100

    menu deroulant

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { PreferencesService } from '../common/service/preferences.service';
+import { MenuDefinition } from 'src/bs-util/data/MenuDefinition';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,19 @@ import { PreferencesService } from '../common/service/preferences.service';
 export class HeaderComponent implements OnInit, OnChanges {
 
   couleurFondPreferee  : string = "yellow";
+
+  myMenuDefs : MenuDefinition[] = [
+    { label : "Basic" , 
+      children : [
+        { label : "tva" , path : "/basic/tva" } ,
+        { label : "calculatrice " , path : "/basic/calculatrice/simple" },
+        { divider : true },
+        { label : "welcome" , path : "/welcome" }
+      ]
+    },
+    { label : "login" , path : "/login" } , 
+    { label : "conversion" , path : "/conversion" }
+    ];
 
   ngOnInit() {
     console.log("ngOnInit() appele avec titre = " + this.titre);
